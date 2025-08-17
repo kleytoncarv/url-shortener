74f27beb739b70f047bd2c96acead8f42af62364
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2025-08-16 21:46:40 -0300

    update comment aoi.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+//public
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+//private
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
