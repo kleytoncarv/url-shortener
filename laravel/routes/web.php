e6eff70ff50c55c6c110d2006f83cec8245e3e3e
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2025-08-19 16:57:24 -0300

    Update web.php
    Adicionado a route de redirecionamento na web

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/s/{shortCode', [RedirectController::class, 'redirect']);
