355fd0f8fa66fc89818a8cf6cab0aade3cf5e6e4
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2025-08-19 18:03:39 -0300

    Update web.php
    Correção do fechamento da chave

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/s/{shortCode', [RedirectController::class, 'redirect']);
+Route::get('/s/{shortCode}', [RedirectController::class, 'redirect']);
