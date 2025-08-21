513ef8efde07e50f1e51ee57e69bf7ba0d3cfd0c
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2025-08-21 14:54:53 -0300

    Create Errors/Views
    Criação das views que exibem os tratamentos de erros para inválidos ou expirados

---
+++ laravel/resources/views/errors/link-expired.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="text-center p-10">
+    <h1 class="text-3xl font-bold text-yellow-600">Link expirado</h1>
+    <p class="mt-4 text-gray-600">Este link já não está mais disponível.</p>
+</div>
+@endsection
---
+++ laravel/resources/views/errors/link-not-found.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="text-center p-10">
+    <h1 class="text-3xl font-bold text-red-600">Link não encontrado</h1>
+    <p class="mt-4 text-gray-600">O link que você tentou acessar não existe.</p>
+</div>
+@endsection
