601e002516f423ebf15374c0ec67da836cb292f3
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2025-08-21 14:28:17 -0300

    Update RedirectController
    Tratando casos de links inválidos ou expirados

--- laravel/app/Http/Controllers/RedirectController.php
+++ laravel/app/Http/Controllers/RedirectController.php
@@ -6,8 +6,19 @@ use App\Models\ShortLink;
 
 class RedirectController extends Controller
 {
-    public function redirect($shortCode){
-        $link = ShortLink::where('short_code', $shortCode)->firstOrFail();
+    public function redirect($shortCode)
+    {
+
+        $link = ShortLink::where('short_code', $shortCode)->first();
+
+        if (!$link) {
+            return response()->view('errors.link-not-found', [],404);
+        }
+
+        if ($link->expire_at && $link->expires_at->isPast()){
+            return response()->view('errors.link-expired', [], 410);
+        }
+        
         return redirect($link->original_url);
     }
 }
